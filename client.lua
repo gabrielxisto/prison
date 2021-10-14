c83c2c13364a8ac4e8023e28dbfe3de213e1db1d
Gabriel Xisto <71442950+Alzon22@users.noreply.github.com>
2021-10-14 14:13:36 -0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@ local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 
-src = Tunnel.getInterface("vrp_prison")
+src = Tunnel.getInterface("prison")
 
 GlobalState.reducaopenalvarrerchao = false
 
